1c411460e6fb90fde893b6d6ef52ea1f94a537b6
The.Korwin@gmail.com <The.Korwin@gmail.com@45335074-22d4-561c-ab8a-e83aa094a423>
2011-03-31 10:39:51 +0000

    Convert abstract entities classes to interfaces

---
+++ trunk/Photogallery/BuisnessLayer/IAlbum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Photogallery
+{
+    public interface IAlbum
+    {
+
+        int AlbumId { get; set; }
+
+        GalleryUser User { get; set; }
+
+        string Title { get; set; }
+
+        IAlbum ParentAlbum { get; set; }
+
+        IEnumerable<IAlbum> ChildAlbums { get; set; }
+
+        IEnumerable<Photo> Photos { get; set; }
+
+        string Description { get; set; }
+
+
+        DateTime CreationDate { get; set; }
+
+        bool IsRootAlbum { get; set; }
+
+        IEnumerable<Tag> AlbumTags { get; set; }
+
+
+
+        void AddComment(Comment comment);
+
+        void DeleteCommentById(int commentId);
+
+
+        void UpdateComment(Comment comment);
+
+
+
+    }
+}
---
+++ trunk/Photogallery/BuisnessLayer/ITag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Photogallery
+{
+    public interface ITag
+    {
+        int TagId { get; set; }
+
+        string TagTitle { get; set; }
+
+    }
+}
