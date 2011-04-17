060f83b26ec1ac14d2e82abc84faeb084a929758
The.Korwin@gmail.com <The.Korwin@gmail.com@45335074-22d4-561c-ab8a-e83aa094a423>
2011-04-17 14:08:04 +0000

    Fix bug - add place entities in Common assembly (was in BL)

---
+++ Photogallery/Common/AbstractEntities/IGalleryUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Web.Security;
+
+namespace Photogallery
+{
+    
+    
+    
+    public interface IGalleryUser
+    {
+        Guid UserId { get; set; }
+
+        string Username { get; set; }
+
+      
+
+       string UserRole { get; set; }
+
+        string UserMail { get; set; } 
+        
+        IAlbum RootAlbum { get; set; }
+
+        IEnumerable<IComment> UserComments { get; }
+
+        string Description { get; set; }
+ 
+
+        void AddComment(IComment comment);
+
+        void DeleteCommentById(int commentId);
+
+
+        void UpdateComment(IComment comment);
+
+    }
+}
---
+++ Photogallery/Common/AbstractEntities/IPhoto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Drawing;
+
+namespace Photogallery
+{
+    public interface IPhoto
+    {
+        int PhotoId { get; set; }
+
+        string PhotoTitle { get; set; }
+
+        IAlbum HostAlbum { get; set; }
+
+        IEnumerable<IComment> PhotoComments { get; set; }
+
+        IEnumerable<ITag> PhotoTags { get; set; }
+
+        IGalleryUser OwningUser { get; set; }
+
+
+        Image PhotoThumbnail { get; set; }
+
+        Image OptimizedPhoto { get; set; }
+
+        Image OriginalPhoto { get; set; }
+
+        string PhotoDescription { get; set; }
+
+        DateTime AdditionDate { get; set; }
+
+        void AddComment(IComment comment);
+		void AddTag(ITag tag);
+		void RemoveTag(ITag tag);
+        void DeleteCommentById(int commentId);
+
+        void UpdateComment(IComment comment);
+
+
+    }
+}
