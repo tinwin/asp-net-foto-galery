28bb7d8e1ee5c1de7575ae1cec6db524290c46c3
The.Korwin@gmail.com <The.Korwin@gmail.com@45335074-22d4-561c-ab8a-e83aa094a423>
2011-04-17 14:08:04 +0000

    Fix bug - add place entities in Common assembly (was in BL)

---
+++ trunk/Photogallery/Common/AbstractEntities/IAlbum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Photogallery
+{
+    public interface IAlbum
+    {
+
+        int AlbumId { get; set; }
+
+        IGalleryUser User { get; set; }
+
+        string Title { get; set; }
+
+        IAlbum ParentAlbum { get; set; }
+
+        IEnumerable<IAlbum> ChildAlbums { get; set; }
+
+        IEnumerable<IPhoto> Photos { get; set; }
+
+        string Description { get; set; }
+
+
+        DateTime CreationDate { get; set; }
+
+        bool IsRootAlbum { get;}
+
+        IEnumerable<ITag> AlbumTags { get; set; }
+
+        IEnumerable<IComment> AlbumComments { get; set; }
+
+
+
+        void AddComment(IComment comment);
+
+        void DeleteCommentById(int commentId);
+
+
+        void UpdateComment(IComment comment);
+
+
+
+    }
+}
---
+++ trunk/Photogallery/Common/AbstractEntities/IComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Photogallery;
+
+namespace Photogallery 
+{
+    public interface IComment
+    {
+        int CommentId { get; set; }
+
+        string Text { get; set; }
+
+        IGalleryUser Author { get; set; }
+
+
+        DateTime AdditionDate { get; set; }
+
+
+    }
+}
