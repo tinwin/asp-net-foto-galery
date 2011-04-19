bd66d98e4a4a4fd4598d02177581b4e59f8b4bfd
The.Korwin@gmail.com <The.Korwin@gmail.com@45335074-22d4-561c-ab8a-e83aa094a423>
2011-04-19 21:21:32 +0000

    Fix photos and album creation; Add useful links for logined user

--- trunk/Photogallery/WebUI/SiteLayout.Master.cs
+++ trunk/Photogallery/WebUI/SiteLayout.Master.cs
@@ -6,6 +6,8 @@ using System.Web;
 using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BuisnessLayer.AbstractControllers;
+using Common;
 using DAL.EFDataProvider;
 using DAL.EFDataProvider.Repositories;
 
@@ -13,9 +15,13 @@ namespace WebUI
 {
 	public partial class SiteLayout : System.Web.UI.MasterPage
 	{
+		public Guid userGuid;
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			var currentUser = Windsor.Instance.Resolve<IEnvironment>().CurrentClient;
 
+			userGuid = (currentUser == null)?Guid.Empty: currentUser.UserId;           
 		    
 		}
 	}
